246b0b157e10fb0f32d1d4f55eb0aed092ac288b
Alexandre NICOL <nemokervi@yahoo.fr>
2018-08-27 09:05:13 +1000

    add services

---
+++ public/src/app/services/common/common.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+	providedIn: 'root'
+})
+
+export class common_service {
+	constructor( private router: Router ){}
+
+	// get from storage
+	get_wallet_id_from_storage(): Promise<any>{
+		return new Promise((resolve, reject)=>{
+			resolve( localStorage.getItem('wallet_id') );
+		})
+	}
+	get_session_from_storage(): Promise<any>{
+		return new Promise((resolve, reject)=>{
+			resolve( localStorage.getItem('session') );
+		})
+	}
+	get_user_from_storage(): Promise<any>{
+		return new Promise((resolve, reject)=>{
+			resolve( localStorage.getItem('user') );
+		})
+	}
+	// clear storage
+	log_out(){
+		localStorage.clear();
+		this.router.navigate(['login']);
+	}
+    update_wallet(){
+        localStorage.removeItem( 'wallet_id' );
+        this.router.navigate(['wallet']);
+    }
+}
